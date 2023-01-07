55ab1bf5fa9ef185f7c33af4bb436d8fb3541ffd
michellelu96 <lu.michelle96@icloud.com>
2023-01-06 22:41:21 -0800

    register page
    working on register page

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,7 +1,41 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom';
+import { Component } from "react";
+import UserService from "../services/userService";
 
-const Home = () =>{
-    
-} 
+
+export default class Home extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            content: ""
+        };
+    }
+
+    componentDidMount(){
+        UserService.getPublicContent.then(
+            response =>{
+                this.setState({
+                    content: response.data
+                });
+            },
+            error =>{
+                this.setState({
+                    content:(
+                        error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>{this.state.content}</h3>
+                </header>
+            </div>
+        )
+    }
+}
--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -53,13 +53,13 @@ class Login extends Component {
         });
 
         this.form.validateAll();
-
-        if(this.checkBtn.context._errors.length === 0){
+        
+        if (this.checkBtn.context._errors.length === 0) {
             AuthService.login(this.state.username, this.state.password).then(
-                () = > {
-                    this.props.router.navigate("/profile");
-                    window.location.reload();
-                },
+              () => {
+                this.props.router.navigate("/profile");
+                window.location.reload();
+              },
                 error => {
                     const resMessage = 
                     (error.reponse &&
@@ -85,10 +85,68 @@ class Login extends Component {
         return(
             <div className="col-md-12">
                 <div className="card card-container">
-                    
+                <img
+                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                    alt="profile-img"
+                    className="profile-img-card"
+                />
+
+                <Form
+                    onSubmit = {this.handleLogin}
+                    ref={c => {
+                        this.form =c;
+                    }}
+                    >
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="username"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername}
+                                validations={(required)}
+                                />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <Input
+                                type="text"
+                                className="form-control"
+                                name="username"
+                                value={this.state.password}
+                                onChange={this.onChangePassword}
+                                validations={(required)}
+                                />
+                        </div>
+
+                        <div className="form-group">
+                            <button
+                                className="btn btn-primary btn-black"
+                                disabled={this.state.loading}
+                                >
+                                    {this.state.loading && (
+                                        <span className="spinner-border spinner-border-sm"/>
+                                    )}
+                                    <span>Login</span>
+                            </button>
+                        </div>
+
+                        {this.state.message &&(
+                            <div className="form-group">
+                                <div className="alert alert-danger" role="alert">{this.state.message}</div>
+                            </div>
+                        )}
+
+                        <CheckButton
+                            style={{display:"none"}}
+                            ref ={c =>{ this.checkBtn = c}}/>
+                    </Form>
                 </div>
             </div>
         )
     }
 }
 
+export default withRouter(login);
